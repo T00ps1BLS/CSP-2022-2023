2b87d44ce7279aded7bb4b1baa5c85b9dcf49d99
T00ps1BLS <113128308+T00ps1BLS@users.noreply.github.com>
2022-10-24 09:53:35 -0700

    Update a.py

--- a.py
+++ a.py
@@ -1,18 +1,17 @@
-from sys import _xoptions
 import turtle as trtl
 import random
 
 tr = trtl.Turtle()
 tr.speed('fastest')
-def reset():
-    tr.penup()
-    tr.goto(randpointx1 - randpointx1 / 2, randpointy2 - randpointy2 / 2)
-    tr.pendown()
-
 randpointx1 = random.randint(-300, 300)
 randpointy1 = random.randint(-300, 300)
+
 randpointx2 = random.randint(-300, 300)
 randpointy2 = random.randint(-300, 300)
+def reset():
+    tr.penup()
+    tr.goto()
+    tr.pendown()
 tr.penup()
 tr.goto(randpointx1, randpointy1)
 tr.pendown()
@@ -21,19 +20,6 @@ tr.goto(randpointx2, randpointy2)
 tr.goto(randpointx1, randpointy2)
 tr.goto(randpointx1, randpointy1)
 
-def resetArea():
-    tr.clear()
-    randpointx1 = random.randint(-300, 300)
-    randpointy1 = random.randint(-300, 300)
-    randpointx2 = random.randint(-300, 300)
-    randpointy2 = random.randint(-300, 300)
-    tr.penup()
-    tr.goto(randpointx1, randpointy1)
-    tr.pendown()
-    tr.goto(randpointx2, randpointy1)
-    tr.goto(randpointx2, randpointy2)
-    tr.goto(randpointx1, randpointy2)
-    tr.goto(randpointx1, randpointy1)
 reset()
 while True:
     tr.seth(random.randint(1, 360))
@@ -42,7 +28,7 @@ while True:
     ypos = tr.ycor()
     if xpos >= randpointx1 or xpos <= randpointx2:
         reset()
-        if ypos >= randpointy1 or xpos <= randpointy2:
+        if ypos >= randpointy1 or ypos <= randpointy2:
             reset()
 
 screen = trtl.Screen()
