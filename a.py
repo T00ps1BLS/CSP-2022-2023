2ba21a86040b9147e5fa196d5d542bf8cb695518
T00ps1BLS <113128308+T00ps1BLS@users.noreply.github.com>
2022-10-25 08:31:28 -0700

    Update a.py

--- a.py
+++ a.py
@@ -1,5 +1,6 @@
 import turtle as trtl
 import random
+from colorsys import hsv_to_rgb
 
 tr = trtl.Turtle()
 tr.speed('fastest')
@@ -10,7 +11,7 @@ randpointx2 = random.randint(-300, 300)
 randpointy2 = random.randint(-300, 300)
 def reset():
     tr.penup()
-    tr.goto()
+    tr.goto(randpointx1 + ((randpointx2 - randpointx1) / 2), randpointy1 + ((randpointy2 - randpointy1) / 2))
     tr.pendown()
 tr.penup()
 tr.goto(randpointx1, randpointy1)
@@ -20,18 +21,25 @@ tr.goto(randpointx2, randpointy2)
 tr.goto(randpointx1, randpointy2)
 tr.goto(randpointx1, randpointy1)
 
+angle = 255
+screen = trtl.Screen()
+screen.colormode(255)
 reset()
 while True:
     tr.seth(random.randint(1, 360))
     tr.forward(random.randint(1, 10))
     xpos = tr.xcor()
     ypos = tr.ycor()
-    if xpos >= randpointx1 or xpos <= randpointx2:
-        reset()
+    if angle != 1:
+        angle -= 1
+        tr.pencolor(angle, 1, 1)
+    if randpointy1 and randpointy2 <= 0:
+        if xpos >= randpointx1 or xpos <= randpointx2:
+            reset()
         if ypos >= randpointy1 or ypos <= randpointy2:
             reset()
 
-screen = trtl.Screen()
+
 
 
 
