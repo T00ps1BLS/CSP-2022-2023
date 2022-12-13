90fe508f9814d9a3abfb3adc53a8dad15bbddc78
T00ps1BLS <113128308+T00ps1BLS@users.noreply.github.com>
2022-12-13 09:51:44 -0800

    Update MainFile.py

--- MainFile.py
+++ MainFile.py
@@ -9,41 +9,53 @@
 # wn = trtl.Screen()
 # trObj = trtl.Turtle()
 num_cats = 3
-catnum = 0
+cat_num = 0
 cat1 = []
 cat2 = []
 cat3 = []
 ## Functions
+
 def new_cat():
-    global catnum, cat1, cat2, cat3
+    global num_cats, cat_num, cat1, cat2, cat3
     words_file = open("initwordlist.txt", "r")
     cat_words = []
-    index = 0
+    
     for line in words_file:
         leader_word = ""
+        index = 0
         if line[index] == "\n":
             break
         while line[index] != "\n":
-            print(index)
+            if line[index] == "1" or line[index] == "2":
+                break
             leader_word += line[index]
             index += 1
         cat_words.append(leader_word)
-    
-    if catnum == 1:
-        cat1 = cat_words
-        return cat1
-    elif catnum == 2:
-        cat2 = cat_words
-        return cat2
-    elif catnum == 3:
-        cat3 = cat_words
-        return cat3
+    new_list = []
+    for word in cat_words:
+        end = False
+        new_word = ""
+        if word == '':
+            end = True
+        if end == True:
+            cat_words.pop()
+        else:
+            new_word += word
+            new_list.append(new_word)
+        
+    if cat_num == 0:
+        cat1 = new_list
+    elif cat_num == 1:
+        cat2 = new_list
+    elif cat_num == 2:
+        cat3 = new_list
+    return cat1, cat2, cat3
 
 
 
 ## Events
 
 
-print(new_cat(), catnum)
+print(new_cat())
 
 # wn.mainloop()
