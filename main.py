bf6c45288862b175e224a415dd0f4130633e4b03
T00ps1BLS <113128308+T00ps1BLS@users.noreply.github.com>
2023-03-16 09:47:54 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import turtle as fred # Imported turtle from https://pythonturtle.org/
+import random # Imported random
 
 screen_w = 1280
 screen_h = 720
@@ -6,7 +7,9 @@ backgroundimg1 = 'TACOBELL-STOREFRONT.png'
 backgroundimg2 = 'KFC-STOREFRONT.png'
 backgroundimg3 = 'ORDER-SCREEN.png'
 employeeimg = 'EMPLOYEE.gif'
+employeeimg1 = 'EMPLOYEE1.gif'
 spchbbl = 'speechbubble.gif'
+menu_items1= ['Taco', 'Taco', 'taco', 'tacos', 'Tacos', 'Taco with Cheese',]
 bgstate = 0
 select = False
 
@@ -15,7 +18,21 @@ wn = fred.Screen()
 wn.setup(width=screen_w, height=screen_h)
 wn.addshape(employeeimg)
 wn.addshape(spchbbl)
-
+emp = fred.Turtle()
+emp.ht()
+emp.pu()
+emp.speed(0)
+spb = fred.Turtle()
+spb.ht()
+# spb.shape(spchbbl)
+spb.pu()
+spb.speed(0)
+spb.goto(0, -323)
+mntrlMAIN = fred.Turtle()
+mntrlMAIN.ht()
+mntrlMAIN.pu()
+mntrlMAIN.speed(0)
+mntrlMAIN.turtlesize(10)
 
 
 def refresh_bg():
@@ -37,6 +54,16 @@ def click(x, y):
         
         print(bgstate)
         refresh_bg()
+
+def menu_turtles():
+    global menu_items1
+    for i in menu_items1:
+        random_taco_num = random.randint(0, 5)
+        index = 0
+        while index != random_taco_num:
+            index += 1
+
+
         
 
 def enter():
@@ -46,21 +73,17 @@ def enter():
         wn.bgcolor('purple')
     elif bgstate == 1:
         wn.bgcolor('red')
+    text_prompt = bgstate
     bgstate = 3
-    emp = fred.Turtle()
-    emp.pu()
-    emp.shape(employeeimg)
-    spb = fred.Turtle()
-    spb.speed(0)
-    spb.pu()
-    spb.shape(spchbbl)
-    spb.turtlesize(0.5)
-    spb.goto(-400, 200)
-    spb.speed(0)
-    spb.write("Hi! What can I get you today?", align='center', font=('Arial', 27, 'bold'))
-
-
     refresh_bg()
+    if text_prompt == 0:
+        spb.write("Hey there! What can I get you today?", align='center', font=('Arial', 27, 'bold'))
+        emp.shape(employeeimg)
+    elif text_prompt == 1:
+        spb.write(".......Hello....... *awkward silence*", align='center', font=('Arial', 27, 'bold'))
+        emp.shape(employeeimg1)
+    emp.st()
+    mntrlMAIN.goto(-350, -50)
     
 
 refresh_bg()
